ac67b0b63acc1a58cffe23288657844cb3a8899a
ineska620 <ineska620@gmail.com>
2024-12-14 16:34:55 +0200

    Add files via upload

---
+++ module_2.6_Камни.py
@@ -0,0 +1,14 @@
+def Kod(number):
+    password = ''
+    for i in range(1, number):
+        for j in range(2, number):
+            if j <= i:
+                continue
+            if number % (i + j) == 0:
+                password += str(i) + str(j)
+    return password
+
+n = int(input('Введите целое число от 3 до 20: '))
+
+result = Kod(n)
+print('Пароль:', result)
